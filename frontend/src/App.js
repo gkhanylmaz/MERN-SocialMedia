2b3ac0006cf3c65eb4807ffcc63dfc3233ece781
Gökhan Yılmaz <gokhanyilmaz997@gmail.com>
2022-12-05 22:30:07 +0300

    created pages and redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Router, Routes } from "react-router-dom";
+import LoginPage from "./pages/loginPage";
+import HomePage from "./pages/homePage";
+import ProfilePage from "./pages/profilePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Router path="/" element={<LoginPage />} />
+          <Router path="/home" element={<HomePage />} />
+          <Router path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
